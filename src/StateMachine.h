b71bc9c848d1d7ff8606b9e030ed027cb73ebf67
jrullan <jerullan@yahoo.com>
2020-06-03 19:24:21 -0400

    Update StateMachine.h
    Indentation

--- src/StateMachine.h
+++ src/StateMachine.h
@@ -17,12 +17,12 @@ class StateMachine
     // When a stated is added we pass the function that represents 
     // that state logic
     State* addState(void (*functionPointer)());
-	State* transitionTo(State* s);
-	int transitionTo(int i);
+    State* transitionTo(State* s);
+    int transitionTo(int i);
 	
     // Attributes
     LinkedList<State*> *stateList;
-	bool executeOnce = true; 	//Indicates that a transition to a different state has occurred
+	  bool executeOnce = true; 	//Indicates that a transition to a different state has occurred
     int currentState = -1;	//Indicates the current state number
 };
 
